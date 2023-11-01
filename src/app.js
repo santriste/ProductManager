c510d868809e40e8fabf58b9cebbad5a89d28e9d
santriste <santiagoestevez722@gmail.com>
2023-11-01 01:46:14 -0300

    Server con express

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express"
+import ProductManager from "./components/productManager.js"
+
+const app = express()
+app.use(express.urlencoded({extended : true}))
+
+const productos = new ProductManager();
+const readProducts = productos.readProducts();
+
+app.get("/products", async (req,res) => {
+    let limit = parseInt(req.query.limit);
+    if (!limit) return res.send(await readProducts)
+    let allProducts = await readProducts
+    let productLimit = allProducts.slice(0, limit)
+res.send(productLimit)
+});
+
+app.get("/products/:id", async (req,res) => {
+    let id = parseInt(req.params.id)
+    let allProducts = await readProducts
+    let productById = allProducts.find(product => product.id === id)
+    res.send(productById)
+})
+
+const PORT=8080; 
+const server = app.listen(PORT, () => {
+    console.log(`Localhost express ${server.address().port}`)
+})
+server.on("error", (error) => console.log(`Server error ${error}`))
--- src/components/ProductManager.js
+++ src/components/ProductManager.js
@@ -1,6 +1,6 @@
 import {promises as fs} from "fs"
 
-class ProductManager {
+export default class ProductManager {
 constructor() {
     this.patch = "./productos.txt";
     this.products = []
@@ -61,11 +61,18 @@ updateProducts = async ({id, ...producto}) => {
 }
 
 
-const productos = new ProductManager();
+//const productos = new ProductManager();
 
-/*productos.addProduct("titulo1", "descripcion1", 1000, "imagen1", "asd123", 5);
-productos.addProduct("titulo2", "descripcion2", 1500, "imagen2", "asd456", 10);
-productos.addProduct("titulo3", "descripcion3", 2000, "imagen3", "asd789", 15);*/
+//  productos.addProduct("titulo1", "descripcion1", 1000, "imagen1", "a1", 5);
+//  productos.addProduct("titulo2", "descripcion2", 1500, "imagen2", "a2", 10);
+//  productos.addProduct("titulo3", "descripcion3", 2000, "imagen3", "a3", 15);
+//  productos.addProduct("titulo4", "descripcion4", 2000, "imagen4", "a4", 15);
+//  productos.addProduct("titulo5", "descripcion5", 2000, "imagen5", "a5", 15);
+//  productos.addProduct("titulo6", "descripcion6", 2000, "imagen6", "a6", 15);
+//  productos.addProduct("titulo7", "descripcion7", 2000, "imagen7", "a7", 15);
+//  productos.addProduct("titulo8", "descripcion8", 2000, "imagen8", "a8", 15);
+//  productos.addProduct("titulo9", "descripcion9", 2000, "imagen9", "a9", 15);
+//  productos.addProduct("titulo10", "descripcion10", 2000, "imagen10", "b1", 15);
 
 //productos.getProducts()
 
@@ -73,10 +80,10 @@ productos.addProduct("titulo3", "descripcion3", 2000, "imagen3", "asd789", 15);*
 
 //productos.deleteProductsById(2)
 
-productos.updateProducts({    title: 'titulo3',
+/*productos.updateProducts({    title: 'titulo3',
     description: 'descripcion3',
     price: 5000,
     thumbnail: 'imagen3',
     code: 'asd789',
     stock: 15,
-    id: 3})
+    id: 3})*/
